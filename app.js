094da781a00a3d775713bdf1297289fc80feb3f8
Nitzan Weinstein <nitzan@battlefin.com>
2020-11-19 05:13:45 +0200

    .

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+import http from 'http';
+import express from 'express';
 	
-	const express = require('express')
-	const port = 8080
+const express = require('express')
+const port = 8080
 
-	let app = express();
-	app.server = http.createServer(app);
+let app = express();
+app.server = http.createServer(app);
 
-	app.get('/', (req, res) => res.send('Hello World 1!'))
+app.get('/', (req, res) => res.send('Hello World 1!'))
 
-	app.server.listen(port, err => {
-	    if(err){
-	        console.log("-->> Error 1: " + err);
-	    }
-	}); 
+app.server.listen(port, err => {
+    if(err){
+        console.log("-->> Error 1: " + err);
+    }
+}); 
 
